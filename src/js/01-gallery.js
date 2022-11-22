8681cf63e76bf4c0a9415d75e41782c109097868
Alexandr-Diachenko <wildfow85@gmail.com>
2022-11-22 23:01:58 +0200

    add image gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const imageContainer = document.querySelector(`.gallery`);
+
+const galleryMarkup = makegalleryItems(galleryItems);
+
+imageContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function makegalleryItems(galleryItems) {
+    return galleryItems
+    .map(({ preview, description, original }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+<img loading="lazy" width="354" height="240" class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    scrollZoom: false,
+});
+
+lightbox.on("shown.simplelightbox", function () {});
+lightbox.on("closed.simplelightbox", function () {});
