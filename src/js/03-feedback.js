abfb4a9d2f646471d72a7b8f5271dc4993f31719
Alexandr-Diachenko <wildfow85@gmail.com>
2022-11-24 23:30:33 +0200

    add video and form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+const throttle = require ("lodash.throttle");
+
+const STORAGE_KEY = `feedback-form-state`;
+const formData = {};
+const refs = {
+    input: document.querySelector(`.feedback-form input`),
+    textarea: document.querySelector(`.feedback-form textarea`),
+    form: document.querySelector(`.feedback-form`),
+}
+
+refs.form.addEventListener(`submit`, onSubmitForm)
+refs.form.addEventListener(`input`, throttle(onInputForm, 500));
+
+populateTextarea()   
+
+function onInputForm(e) {
+    formData[e.target.name] = e.target.value;
+    const formDataJSON = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, formDataJSON)
+
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    e.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+    
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+        const ItemMessage = JSON.parse(saveMessage);
+        refs.input.value = ItemMessage.email;
+        refs.textarea.value = ItemMessage.message;
+        console.log(on);
+    }
+}
